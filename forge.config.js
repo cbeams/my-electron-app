efa7fd8cd837df4a45e34b280f712e390ee2f665
Chris Beams <chris@beams.io>
2025-07-20 15:48:13 +0200

    Set up GitHub Publisher
    Per https://www.electronjs.org/docs/latest/tutorial/tutorial-publishing-updating#setting-up-the-github-publisher

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'cbeams',
+          name: 'my-electron-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
